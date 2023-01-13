4d36be0d3184d5e1e41bed186c70e4b237477206
Axelle <guinaudeau.ion1516@gmail.com>
2023-01-13 15:53:21 +0100

    :art: improve css

--- sw.js
+++ sw.js
@@ -1,45 +1,26 @@
-// mise en cache avant de lancer l'activation
-var CACHE_NAME = 'watch-patata-cache-v1';
-var urlsToCache = [
-    '/',
-    '/style.css',
-    '/index.js',
-]
+const staticPomodoro = "dev-coffee-site-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/index.js",
+  "/icons/rainbow.png",
+  "/audio/bell.mp3"
+];
 
-// intall
-self.addEventListener('install', evt => {
-    // Perform install steps
-    evt.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => {
-            // console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-        })
-    );
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticPomodoro).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
 
-    // console.log("service worker has been installed!");
-})
-
-// activation
-self.addEventListener('activate', evt => {
-    // nettoyer les anciens fichiers pour un cache propre et a jour :)
-    evt.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhiteList.indexOf(cacheName) === -1){
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-
-    // console.log("service worker has been activate!");
-})
-
-// declenche lorsqu une requete HTTP est emise par l application
-// permet d intercepter des requetes et d y repondre de façon personnalisee
-self.addEventListener('fetch', evt => {
-    // console.log("Ressource récupéreée" + evt.request.url);
-})
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
